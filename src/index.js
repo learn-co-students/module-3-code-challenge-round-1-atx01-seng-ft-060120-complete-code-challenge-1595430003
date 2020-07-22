e77aaa346689e10ff1d6de2d9ab9e5f420049ed6
VenkatNan <nanditavenkat@gmail.com>
2020-07-22 11:53:07 -0500

    12 noon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
+let imageId = 5658 //Enter the id from the fetched image here
+  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`;
+  const likeURL = `https://randopic.herokuapp.com/likes/`;
+  const commentsURL = `https://randopic.herokuapp.com/comments/`;
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta');
 
-  let imageId = 1 //Enter the id from the fetched image here
+  fetch(imageURL)
+    .then((res) => res.json())
+    .then((json) => postImg(json));
+});
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+function postImg(data){
+  const img = document.getElementById('image');
+  img.src = data.url;
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+  const name = document.getElementById('name');
+  name.innerText = data.name;
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+  const like = document.getElementById('likes');
+  like.innerText = data.like_count;
 
-})
+  const word = document.getElementById('comments');
+  data.comments.forEach((e) => {
+
+    let li = document.createElement('li');
+    li.innerText = e.content;
+    word.appendChild(li);
+  });
+
+  const btn = document.getElementById('like_button');
+  btn.addEventListener("click", (e)=>{
+    data.like_count++ ;
+    like.innerText = data.like_count;
+        addLike(data);
+  })
+}
+
+function addLike(data){
+  fetch (likeURL, {
+    method: "PATCH",
+    headers: {
+      "Accept": 'application/json',
+      "Content-Type": 'application/json'
+    },
+    body: JSON.stringify ({
+      image_id: data.id ,
+      likes: data.like_count
+    })
+  });
+}
