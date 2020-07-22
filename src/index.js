2de0776751b05180e3c8d7ebfd5b574b88209d1b
smithspencer817 <smithspencer817@gmail.com>
2020-07-22 12:01:54 -0500

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,12 +1,96 @@
+let imageId = 5659 //Enter the id from the fetched image here
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const likeURL = `https://randopic.herokuapp.com/likes/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+    doEverything();
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+});
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+function doEverything() {
+  fetch(imageURL)
+  .then(res => res.json())
+  .then(img => renderImageInfo(img));
+};
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+function renderImageInfo(img) {
 
-})
+  // image
+  imageDisplay = document.getElementById("image");
+  imageDisplay.src = img.url;
+
+  // name
+  imageName = document.getElementById("title-name");
+  imageName.innerText = img.name;
+
+  // likes
+  imageLikes = document.getElementById("likes");
+  imageLikes.innerText = img.like_count;
+
+  // comments
+  imageCommentsList = document.getElementById("comments");
+
+  for (const comment of img.comments) {
+    imageComment = document.createElement("li");
+    imageComment.innerHTML = comment.content
+
+    imageCommentsList.appendChild(imageComment);
+  };
+
+  // add button functionality
+
+  likeButton = document.getElementById("like_button");
+  likeButton.addEventListener("click", (e) => {
+    addLike(img);
+  });
+
+  commentForm = document.getElementById("comment_form");
+  commentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    renderComment(img, imageCommentsList);
+    commentForm.reset();
+  });
+};
+
+function renderComment(json, commentsList) {
+  input = document.getElementById("comment_input").value;
+  newComment = document.createElement("li");
+  newComment.innerText = input;
+  commentsList.appendChild(newComment);
+
+  const data = {
+    image_id: json.id,
+    content: input
+  }
+
+  fetch(commentsURL, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+};
+
+function addLike(json) {
+  const likes = document.getElementById("likes")
+  newLikes = (parseInt(likes.innerText) + 1).toString()
+  likes.innerText = newLikes;
+
+  const data = {
+    image_id: json.id,
+    like_count: newLikes
+  };
+
+  fetch(likeURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+};
